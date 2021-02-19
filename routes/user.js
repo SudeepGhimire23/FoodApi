cab04f71224eb3f76bac7cd1938e38cb02537ed3
bulls <sudeep.ghimire23@gmail.com>
2021-02-19 11:54:22 +0545

    food api 1

---
+++ routes/user.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const  router = express.Router();
+
+const {
+ 
+  getuser,
+  createUser,
+  getuserById,
+  deleteUser,
+  } = require("../controllers/user");
+
+  const { protect } = require("../middleware/auth");
+
+  router
+  .route("/")
+  .get(protect,getuser)
+  .post(protect,createUser);
+
+//   router
+//   .route("/:id/photo")
+//   .put(protect, StudentPhotoUpload);
+
+  router
+  .route("/:id")
+  .get(protect,getuserById)
+  .delete(protect, deleteUser);
+
+
+  module.exports = router
